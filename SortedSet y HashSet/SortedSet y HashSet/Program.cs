665b30e33f59b3abe933298fd4554f337f198b3c
Juan Cruz Ledesma <g-ohan2009@hotmail.com>
2022-10-12 23:41:09 -0300

    HashSet

--- SortedSet y HashSet/SortedSet y HashSet/Program.cs
+++ SortedSet y HashSet/SortedSet y HashSet/Program.cs
@@ -13,32 +13,36 @@ namespace SortedSet_y_HashSet
             //string frase;
             //SortedSet<string> Diccionario = new SortedSet<string>();
 
-            //Diccionario.Add("hola");
-            //Diccionario.Add("adios");
-            //Diccionario.Add("yo soy");
-            //Diccionario.Add("hermoso");
-            //Diccionario.Add("padre");
-            //Diccionario.Add("hermano");
-            //Diccionario.Add("perro");
-            //Diccionario.Add("gato");
-
-            //Console.WriteLine("Ingresa una frase");
+            //Console.WriteLine("Ingrese frase al alzar. Si ya no desea ingresar mas frases, no escriba nada y de ENTER.");
             //frase = Console.ReadLine();
+            //Diccionario.Add(frase);
 
-            //if (Diccionario.Contains(frase))
+            //while (frase != "")
             //{
-            //    Console.WriteLine($"La frase {frase}, se encuentra en el diccionario");
-            //}
-            //else
-            //{
-            //    Console.WriteLine($"No se ha encontrado la frase '{frase}' en el Diccionario");
+            //    frase = Console.ReadLine();
+
+            //    Diccionario.Add(frase);
+
+            //    if (Diccionario.Contains(frase))
+            //    {
+            //        Console.WriteLine("La frase ingresada, fue escrita anteriormente");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("La frase ingresada, NO fue escrita anteriormente");
+            //    }
+
             //}
 
-            ////conbsola mantenga abierta
-            //Console.ReadKey(); 
-            
+            //if (frase == "")
+            //{
+            //    Console.WriteLine("No ha escrito ninguna frase.");
+            //}
+
+            //Console.ReadKey();
+
             ///// =============================== /////////////
-            
+
             string frase;
             HashSet<string> Diccionario = new HashSet<string>();
 
